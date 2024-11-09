ee751618f92c3ff838f9e4bac31efbe5f93e588d
Jan Klimo <jan.klimo@gmail.com>
2024-11-09 12:37:00 +0700

    Log to /logs

--- run.rb
+++ run.rb
@@ -5,16 +5,18 @@ require 'logger'
 require 'dotenv'
 require 'httparty'
 require 'json'
+require 'fileutils'
 
 Dotenv.load
 
 class Runner
   def initialize
-    @logger = Logger.new($stdout)
-    @logger.level = Logger::INFO
+    setup_logging
   end
 
   def run
+    @logger.info "Starting validator check at #{Time.now}"
+
     if jailed?
       @logger.info 'Validator jailed, unjailing...'
       execute_unjail_command
@@ -25,10 +27,31 @@ class Runner
 
   private
 
+  def setup_logging
+    # Create logs directory if it doesn't exist
+    FileUtils.mkdir_p('logs')
+
+    # Create a log file with today's date
+    log_file = File.join('logs', "validator_#{Time.now.strftime('%Y%m%d')}.log")
+
+    # Create a multi-logger that writes to both STDOUT and file
+    @logger = Logger.new(MultiIO.new($stdout, File.open(log_file, 'a')))
+    @logger.level = Logger::INFO
+
+    # Customize the log format
+    @logger.formatter = proc do |severity, datetime, _progname, msg|
+      date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
+      "[#{date_format}] #{severity}: #{msg}\n"
+    end
+  end
+
   def jailed?
     node = validators_data.detect { |data| data['validator'] == ENV['VALIDATOR'] }
 
-    return false unless node
+    unless node
+      @logger.warn "Validator #{ENV['VALIDATOR']} not found in validators data"
+      return false
+    end
 
     node['isJailed']
   end
@@ -38,7 +61,16 @@ class Runner
     headers = { 'Content-Type' => 'application/json' }
     body = { type: 'validatorSummaries' }.to_json
 
-    HTTParty.post(url, headers:, body:)
+    @logger.debug "Fetching validators data from #{url}"
+    response = HTTParty.post(url, headers:, body:)
+
+    if response.success?
+      @logger.debug 'Successfully retrieved validators data'
+      response
+    else
+      @logger.error "Failed to fetch validators data: #{response.code} - #{response.body}"
+      []
+    end
   end
 
   def execute_unjail_command
@@ -46,6 +78,7 @@ class Runner
     command = "~/hl-node --chain Testnet --key #{key} send-signed-action '{\"type\": \"CSignerAction\", \"unjailSelf\": null}'"
 
     begin
+      @logger.info 'Executing unjail command...'
       stdout, stderr, status = Open3.capture3(command)
 
       if status.success?
@@ -62,6 +95,21 @@ class Runner
       @logger.error e.backtrace.join("\n")
       false
     end
+  end
+end
+
+# Helper class to write to multiple IO objects
+class MultiIO
+  def initialize(*targets)
+    @targets = targets
+  end
+
+  def write(*args)
+    @targets.each { |t| t.write(*args) }
+  end
+
+  def close
+    @targets.each(&:close)
   end
 end
 
